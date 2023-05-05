5c72c03184c3fba75515e2faac849c9d804727ba
Dan Donahue <ddonahuex@gmail.com>
2023-05-05 14:20:58 -0400

    Add GKE tf

---
+++ iac/tf/gcp/gke/vpc.tf
@@ -0,0 +1,39 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  credentials = "gcp-creds.ini"
+}
+
+// Modules _must_ use remote state. The provider does not persist state.
+terraform {
+  backend "kubernetes" {
+    secret_suffix     = "providerconfig-default"
+    namespace         = "default"
+    in_cluster_config = true
+  }
+}
+
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "${var.project_id}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.project_id}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+}
