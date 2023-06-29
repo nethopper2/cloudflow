b3ccb3f73b55cc80794b708da41d82597a8262b8
Dan Donahue <ddonahuex@gmail.com>
2023-06-29 10:03:07 -0400

    Change namespace

--- iac/tf/aws/vpc-aws/main.tf
+++ iac/tf/aws/vpc-aws/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 terraform {
   backend "kubernetes" {
     secret_suffix     = "providerconfig-default"
-    namespace         = "nethopper"
+    namespace         = "default"
     in_cluster_config = true
   }
 }
