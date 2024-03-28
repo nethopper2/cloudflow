fcaaee008430ad70c2e242eb6de5208910a21c5b
mikeNetHopper <98332038+mikeNetHopper@users.noreply.github.com>
2024-03-28 10:09:05 -0400

    Update main.tf

--- iac/tf/aws/route53/main.tf
+++ iac/tf/aws/route53/main.tf
@@ -15,10 +15,10 @@ provider "aws" {
 # in-cluster tf
 #   - both provider & terraform blocks
 #
-provider "kubernetes" {
+#provider "kubernetes" {
 #  host                   = module.eks.cluster_endpoint
 #  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-}
+#}
 
 terraform {
   backend "kubernetes" {
