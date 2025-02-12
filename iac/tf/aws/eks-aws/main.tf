0a0c255df0b7dce451ec5c4ab6d88393de1bc507
Dan Donahue <ddonahuex@gmail.com>
2025-02-12 08:37:52 -0500

    Update main.tf

--- iac/tf/aws/eks-aws/main.tf
+++ iac/tf/aws/eks-aws/main.tf
@@ -17,7 +17,7 @@ provider "kubernetes" {
 terraform {
   backend "kubernetes" {
     secret_suffix     = "providerconfig-default"
-    namespace         = "nethopper"
+    namespace         = "kaops"
     in_cluster_config = true
   }
 }
